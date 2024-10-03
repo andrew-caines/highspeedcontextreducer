c3f7783bb63dffefa7506bf81e6d6facf6ad1916
Andrew Caines <andrew.p.caines@gmail.com>
2024-10-03 13:19:21 -0600

    Typescripted the Express server, made types, and Generic type for Messages

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
-const http = require("http");
-const Server = require("socket.io");
+import http from "http";
+import Server from "socket.io";
 const httpServer = http.createServer();
 const io = new Server.Server(httpServer, {
     cors: {
@@ -7,34 +7,75 @@ const io = new Server.Server(httpServer, {
         credentials: true
     }
 });
-const PORT = 3666;
 
+//MARK: Constants
+const PORT = 3666;
+const SERVER_HEARTBEAT_DELAY = 100; //This is the ms delay between heart beats
 let usersConnected = 0; //Current count of users online
+//MARK: Globals
 let serverHB = 0; //Heart Beat count.
+
+//MARK: Types
+type HeartbeatPayload = {
+    serverHB: number,
+    alert: number
+};
+
+type ProgramMaster = true;
+type ProgramUpdate = {
+    value: number
+};
+
+enum MessageType {
+    usercount = 'userCount',
+    heartbeat = 'heartbeat',
+    updateProgressBar = 'updateProgress',
+    programMaster = 'progMaster',
+    programUpdate = 'progUpdate',
+    disconnect = 'disconnect',
+};
+interface Action<T> {
+    type: MessageType,
+    payload: T
+}
+
+type PayloadMap = {
+    [MessageType.usercount]: number,
+    [MessageType.heartbeat]: HeartbeatPayload,
+    [MessageType.programMaster]: ProgramMaster,
+    [MessageType.programUpdate]: ProgramUpdate,
+    [MessageType.updateProgressBar]: number,
+    [MessageType.disconnect]: undefined
+};
+
+//MARK: Functions & Handlers
+function emitAction<T extends MessageType>(type: T, payload: PayloadMap[T]) {
+    io.emit("action", { type, payload });
+}
 
 //Start the heart beat. One each heartbeat the server will emit a payload that tells client which box to highlight.
 setInterval(() => {
-    io.emit("action", { type: 'heartbeat', payload: { serverHB: serverHB, alert: Math.floor(Math.random() * 8) + 1 } });
+    emitAction(MessageType.heartbeat, { serverHB: serverHB, alert: Math.floor(Math.random() * 8) + 1 });
     serverHB++;
 }, 150);
 
 //Listen to Socket messages here.
 io.on("connection", (socket) => {
     if (usersConnected === 0) {
-        io.emit("action", { type: 'progMaster', payload: true }); //First user connected is given a slider bar to interact with.
+        emitAction(MessageType.programMaster, true);//First user connected is given a slider bar to interact with.
     }
     usersConnected++;
     console.log(`User Connected: ${usersConnected} users online.`);
 
-    io.emit("action", { type: 'userCount', payload: usersConnected });
+    emitAction(MessageType.usercount, usersConnected);
 
-    socket.on('progUpdate', (data) => {
-        io.emit("action", { type: 'updateProgress', payload: data.value });
+    socket.on(MessageType.programUpdate, (data) => {
+        emitAction(MessageType.updateProgressBar, data.value);
     });
 
-    socket.on('disconnect', (data) => {
-        io.emit("action", { type: 'userCount', payload: usersConnected });
+    socket.on(MessageType.disconnect, (data) => {
         usersConnected--;
+        emitAction(MessageType.usercount, usersConnected);
         console.log(`User Disconnected: ${usersConnected} users online.`);
     });
 });
--- src/App.js
+++ src/App.js
@@ -1,44 +1,93 @@
-import React, { useContext } from 'react';
-import { Alert, Container, Row, Col, Card, CardBody, CardHeader, Progress } from 'reactstrap';
-import { GlobalStateContext } from './GlobalState';
+import React, { useContext } from "react";
+import { Alert, Container, Row, Col, Card, CardBody, CardHeader, Progress } from "reactstrap";
+import { GlobalStateContext } from "./GlobalState";
 
 function App() {
   const { state, handleSlider } = useContext(GlobalStateContext);
 
   return (
     <Container fluid>
-      <Row>
+      <Row className="mt-3">
         <Col md={12}>
           <Card>
             <CardHeader>Messages {state.usersOnline} users Online.</CardHeader>
             <CardBody>
-              <p>We have received {state.heartBeats} heartbeats from the server  The server Heartbeat count is:{state.serverHB}</p>
+              <p>
+                We have received {state.heartBeats} heartbeats from the server The server Heartbeat
+                count is:{state.serverHB}
+              </p>
               <Row>
-                <Col><Alert color={state.currentAlert === 1 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
-                <Col><Alert color={state.currentAlert === 2 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
-                <Col><Alert color={state.currentAlert === 3 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
+                <Col>
+                  <Alert color={state.currentAlert === 1 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
+                <Col>
+                  <Alert color={state.currentAlert === 2 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
+                <Col>
+                  <Alert color={state.currentAlert === 3 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
               </Row>
               <Row>
-                <Col><Alert color={state.currentAlert === 4 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
-                <Col><Alert color={state.currentAlert === 5 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
-                <Col><Alert color={state.currentAlert === 6 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
+                <Col>
+                  <Alert color={state.currentAlert === 4 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
+                <Col>
+                  <Alert color={state.currentAlert === 5 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
+                <Col>
+                  <Alert color={state.currentAlert === 6 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
               </Row>
               <Row>
-                <Col><Alert color={state.currentAlert === 7 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
-                <Col><Alert color={state.currentAlert === 8 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
-                <Col><Alert color={state.currentAlert === 9 ? "danger" : "primary"}>Let's See how Syncronized this stays!</Alert></Col>
+                <Col>
+                  <Alert color={state.currentAlert === 7 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
+                <Col>
+                  <Alert color={state.currentAlert === 8 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
+                <Col>
+                  <Alert color={state.currentAlert === 9 ? "danger" : "primary"}>
+                    Let's See how Syncronized this stays!
+                  </Alert>
+                </Col>
               </Row>
               <Row>
                 <Col>
                   <Progress animated value={state.prog} />
-                  {state.isProgMaster ? <input type="range" min="1" max="100" value={state.prog} className="slider" id="myRange" onChange={handleSlider}></input> : null}
+                  {state.isProgMaster ? (
+                    <input
+                      type="range"
+                      min="1"
+                      max="100"
+                      value={state.prog}
+                      className="slider"
+                      id="myRange"
+                      onChange={handleSlider}
+                    ></input>
+                  ) : null}
                 </Col>
               </Row>
             </CardBody>
           </Card>
         </Col>
       </Row>
-    </Container >
+    </Container>
   );
 }
 
